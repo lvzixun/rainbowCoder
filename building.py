a55ae59ad855cbc1852a1ccae9ede53b6db582c4
zixun <lvzixun@gmail.com>
2014-07-14 10:37:03 +0800

    change to cmd

---
+++ building.py
@@ -0,0 +1,39 @@
+import sys
+import gen
+import mgr
+
+
+class Building(object):
+  def __init__(self):
+    self.rainbow = gen.GeneratedRainbowCoder()
+    self.cmd_instruct = {
+      "building_all": 
+        lambda: self.rainbow.building_all(),
+      "building_index": 
+        lambda: self.rainbow.building_index_md(),
+      "building_update": 
+        lambda: self.rainbow.building_update(),
+    }
+
+  def use_page(self):
+    cmd = ""
+    for (k, v) in self.cmd_instruct.items():
+      cmd += "    " + k + "\n"
+    print('use_page: \n python cmd.py <cmd>\n cmd list:\n' + cmd)
+    exit()
+
+  def run(self, cmd):
+    if not self.cmd_instruct.has_key(cmd):
+      self.use_page()
+    else:
+      func = self.cmd_instruct[cmd]
+      func()
+
+
+building = Building()
+
+argv = sys.argv
+if len(argv) < 2:
+  building.use_page()
+
+building.run(argv[1])
